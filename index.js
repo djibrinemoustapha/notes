c347152051178ee3659f9a5e60166a0c5edcca28
djibrinemoustapha <djibrinemoustaha53@gmail.com>
2025-08-27 10:50:04 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
- const express = require('express')
+require('dotenv').config()
+const express = require('express')
+ const Note = require('.models/note')
  const app = express()
 
 
@@ -90,3 +92,4 @@ const PORT = process.env.PORT || 3001
  app.listen(PORT, () => {
  console.log(`Server running on port ${PORT}`)
  })
+
